396ea35f0c59741cd05362c0efc7d4c590870ae4
Syed Zakeer Hussain <zakeer@zakeer.me>
2025-03-15 19:14:57 +0530

    feat: implemented shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { useState } from 'react';
+import { Button } from '~/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='p-8 flex gap-2'>
+      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
+      <Button className='px-8' onClick={() => setCount(count + 1)}>
+        {count}
+      </Button>
+    </div>
   );
 }
 
